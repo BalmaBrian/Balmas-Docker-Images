3edc6aaf5258d68edabb5c465d438dcf20efaf94
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-04 15:45:01 -0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# REM Building neovim containers
+docker build -t neovim-base ./neovim-base/Dockerfile
+
+# REM Building spacevim conatiners
+docker build -t spacevim-base:alpine ./alpine/Dockerfile
+docker build -t spacevim-base:archlinux ./archlinux/Dockerfile
+docker build -t spacevim-base:centos ./centos/Dockerfile
+docker build -t spacevim-base:clearlinux ./clearlinux/Dockerfile
+docker build -t spacevim-base:fedora ./fedora/Dockerfile
+docker build -t spacevim-base:ubuntu ./ubuntu/Dockerfile
+
+# REM Building ytdl-layr containers
+docker build -t ytdl-layr-backend ./ytdl-layr-backend/Dockerfile
+docker build -t ytdl-layr-frontend ./ytdl-layr-frontend/Dockerfile
