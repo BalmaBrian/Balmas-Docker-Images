429fac437ff2c77859297fef8f9f346e2842aa31
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-04 15:46:44 -0800

    Create backend.js

---
+++ ytdl-layr-backend/src/backend.js
@@ -0,0 +1,26 @@
+import {Component, primaryIdentifier, attribute, method, expose} from '@layr/component';
+import {ComponentHTTPServer} from '@layr/component-http-server';
+import {exec, spawn} from 'child_process';
+
+class spawnCMD extends Component {
+  // We need a primary identifier
+  @expose({get: true, set: true}) @primaryIdentifier() id;
+
+  // Exposing the cmd value
+  @expose({get: true, set: true}) @attribute('string') cmd = '';
+
+  // The business logic
+  @expose({call: true}) @method() spawnCMD() {
+    exec = this.cmd.split(' ');
+    command = exec[0];
+    args = exec.splice(1,-1);
+    youtubedl = spawn(command, args);
+    // youtubedl.on("close", code => {
+    //   console.log(`child process exited with code ${code}`);
+    // });
+  }
+}
+
+// We serve the Counter through a ComponentHTTPServer
+const server = new ComponentHTTPServer(spawnCMD, {port: 3000});
+server.start();
